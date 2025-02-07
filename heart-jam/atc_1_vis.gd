723b568743247b3d8b2832fdf3a0ed31ed68c888
Ænthroppe <jamison.bassett@gmail.com>
2025-02-07 13:17:27 +0100

    more theme work + unfinished prompt moved to better position

--- heart-jam/atc_1_vis.gd
+++ heart-jam/atc_1_vis.gd
@@ -4,6 +4,7 @@ class_name Act1Vis
 @onready var base = $Room
 @onready var filter = $Room2
 @onready var cracks_shader = $Cracks.get_material()
+@onready var cracks_shader_2 = $Cracks2.get_material()
 var switch = true
 
 func glitch(loops: float):
@@ -13,6 +14,7 @@ func glitch(loops: float):
 	#var tween = create_tween().set_loops(5)
 	#tween.tween_callback(flicker).set_delay(1.75)
 	cracks_shader.set_shader_parameter("size", loops)
+	#cracks_shader_2.set_shader_parameter("size", loops)
 
 func flicker():
 	print("flicker")
